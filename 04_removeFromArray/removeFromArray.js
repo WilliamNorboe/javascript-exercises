20d1c45c83c1eed98da929b7e60330784ffebba7
William Norboe <william.norboe@gmail.com>
2023-06-20 11:08:23 -0700

    added 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(ar) {
+    for(let j = 1; j < arguments.length; ++j){
+        let i = -1;
+        for(i = 0; i < ar.length; ++i){
+            if(ar[i] === arguments[j]){
+                break;
+            }
+        }
+        if(i != -1){
+            ar.splice(i, 1);
+        }
+    }
 
+    return ar;
 };
 
 // Do not edit below this line
