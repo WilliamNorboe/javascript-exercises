eea4f5b89e69975fce23fca2e8db0a7ddf07a8a1
William Norboe <william.norboe@gmail.com>
2023-06-21 11:26:54 -0700

    solved 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+function isLetter(str) {
+    return str.length === 1 && (str.match(/[a-z]/i) || str.match(/[0-9]/i));
+  }
 
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    for(let i = 0, j = str.length-1; j != i; ++i, --j){
+        if(!isLetter(str[i])){
+            j++;
+            continue
+        }
+        else if(!isLetter(str[j])){
+            i--;
+            continue
+        }
+        if(str[i] != str[j]){
+            return false;
+        }
+    }
+    return true;
 };
 
+
+console.log(palindromes("Animal loots foliated detail of stool lamina."));
 // Do not edit below this line
 module.exports = palindromes;
