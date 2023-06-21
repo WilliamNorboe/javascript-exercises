70524101b438174d767b11ec65efb3f5c59d8a33
William Norboe <william.norboe@gmail.com>
2023-06-21 12:19:46 -0700

    Solved the rest of the problems

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let result = people.reduce((oldest, person) =>{
+        if(!person.yearOfDeath || !oldest.yearOfDeath){
+            if(person.yearOfBirth < oldest.yearOfBirth){
+                oldest = person;
+            }
+        }
+        else if((person.yearOfDeath - person.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth)){
+            oldest = person;
+        }
+        return oldest;
+    });
+    return result;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
