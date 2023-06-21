cba84aef80e3d119f2511c0ec33680737d6cf6f6
William Norboe <william.norboe@gmail.com>
2023-06-21 11:02:40 -0700

    added solution to 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  total = 0;
+	a.forEach(element => {
+    total += element;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+  total = 1;
+  argumentsAR = Array.from(arguments);
+  argumentsAR.forEach(element => {
+    total *= element;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, p) {
+	return Math.pow(num, p);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 1 || num == 0){
+    return 1;
+  }
+  return num * factorial(num-1);
 };
-
+// console.log(sum([5,2,7]));
 // Do not edit below this line
 module.exports = {
   add,
